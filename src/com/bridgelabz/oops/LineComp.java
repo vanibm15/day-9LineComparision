ce8dd31707b3139ea0f47005786073de6ec6e018
vanibm15 <vanibm15@gmail.com>
2022-04-29 14:58:52 +0530

    added:comparision of two line

--- src/com/bridgelabz/oops/LineComp.java
+++ src/com/bridgelabz/oops/LineComp.java
@@ -9,9 +9,34 @@ public class LineComp {
          System.out.println("length of line:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")==>"+lenghtofpoints);
          lenghtofpoints=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
          System.out.println("length of line:"+lenghtofpoints);
+
      }
+    //2nd method
+     void Compare(){
+         int x1, y1, x2, y2;
+         int x3, y3, x4, y4;
+         double lenghtofpoints1, lenghtofpoints2;
+         x1 = 1;
+         y1 = 1;
+         x2 = 4;
+         y2 = 4;
+         x3 = 2;
+         y3 = 3;
+         x4 = 5;
+         y4 = 5;
+         lenghtofpoints1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+         lenghtofpoints2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+
+         if (lenghtofpoints1 == lenghtofpoints2) {
+             System.out.println("length of both line are equal " + lenghtofpoints1 + "=" + lenghtofpoints2);
+         } else {
+             System.out.println("length of both lines are not equal");
+         }
+     }
+
      public static void main(String[] args) {
          LineComp obj1=new LineComp();
          obj1.method1();
+         obj1.Compare();
     }
 }
